3ace6f9565c4d951462f7964c9abc889f2654b94
tle18 <tle18@uiowa.edu>
2025-02-12 18:08:56 -0600

    added login and sign up button, modified navbar changed button names on navbar modified profile button

--- src/App.vue
+++ src/App.vue
@@ -2,48 +2,66 @@
 </script>
 
 <template>
-  <div>
-    <nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">TalentHub</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Jobs</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Saved Jobs
-              </a>
-              <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Action</a></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a class="dropdown-item" href="#">Something else here</a></li>
-              </ul>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-            </li>
-            <li>
-              <form class="d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
-              </form>
-            </li>
-          </ul>
-          <a><img src="./assets/user.png" style="max-height: 35px;"/></a>
+  <nav class="navbar navbar-expand-lg bg-primary navbar-dark">
+    <div class="container-fluid">
+      <!-- Logo -->
+      <a class="navbar-brand fw-bold" href="#">TalentHub</a>
+
+      <!-- Mobile Toggle Button -->
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <!-- Left Side Links -->
+        <ul class="navbar-nav me-auto">
+          <li class="nav-item">
+            <a class="nav-link active" href="#">Find Jobs</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Post a Job</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Companies</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Career Advice</a>
+          </li>
+        </ul>
+
+        <!-- Search Bar -->
+        <form class="d-flex me-3">
+          <input class="form-control me-2" type="search" placeholder="Search jobs, companies..." />
+          <button class="btn btn-outline-light" type="submit">Search</button>
+        </form>
+
+        <!-- Profile & Auth Buttons -->
+        <div class="d-flex align-items-center">
+          <button class="btn btn-outline-light me-2">Login</button>
+          <button class="btn btn-warning">Sign Up</button>
+
+          <!-- Profile Dropdown -->
+          <div class="dropdown ms-3">
+            <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown">
+              <img src="@/assets/user.png" style="max-height: 30px;" alt="User" class="rounded-circle" />
+            </button>
+            <ul class="dropdown-menu dropdown-menu-end">
+              <li><a class="dropdown-item" href="#">Profile</a></li>
+              <li><a class="dropdown-item" href="#">Saved Jobs</a></li>
+              <li><a class="dropdown-item" href="#">Settings</a></li>
+              <li><hr class="dropdown-divider"></li>
+              <li><a class="dropdown-item text-danger" href="#">Logout</a></li>
+            </ul>
+          </div>
         </div>
       </div>
-    </nav>
-  </div>      
+    </div>
+  </nav>
 </template>
 
 <style scoped>
+.navbar-brand {
+  font-size: 1.5rem;
+}
 </style>
+
